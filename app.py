23286b40bdde04deb733837742cef98a9ff2e0f9
fornerobj <fornerobj@gmail.com>
2024-04-17 00:20:30 -0500

    File upload works and saves to uploads folder

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        print("post method")
+        image = request.files['file']
+        image.save('uploads/' + secure_filename(image.filename))
     return render_template("index.html") 
 
 if __name__ == "__main__":
