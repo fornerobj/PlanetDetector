0865fdb77f75f7ef1cfd063d82f4de6360410ce0
fornerobj <fornerobj@gmail.com>
2024-04-17 00:07:09 -0500

    Added post handling and file upload button

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def index():
+    if request.method == 'POST':
+        print("post method")
     return render_template("index.html") 
 
 if __name__ == "__main__":
