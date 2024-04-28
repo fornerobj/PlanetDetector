9ce426ede5509162054f302558980852ee8c9315
Brandon Fornero <fornerobj@gmail.com>
2024-04-28 15:45:23 -0500

    Minor Bootstrap Styling

--- app.py
+++ app.py
@@ -2,21 +2,30 @@ from os import pread
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import send_from_directory
 from werkzeug.utils import secure_filename
 
 from prediction_utils import predict_image
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
 
 @app.route("/", methods=['POST', 'GET'])
 def index():
+    prediction = None
+    filename = None
     if request.method == 'POST':
         uploaded_file = request.files['file']
         if uploaded_file.filename != '':
             img_path = 'uploads/' + secure_filename(uploaded_file.filename)
             uploaded_file.save(img_path)
-            print(predict_image(img_path))
-    return render_template("index.html") 
+            prediction = predict_image(img_path)
+            filename = uploaded_file.filename
+    return render_template("index.html", prediction=prediction, filename=filename)
+
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == "__main__":
     app.run()
